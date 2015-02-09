c96d18af2fed5fbcc39c72d02b7a1561b24830f1
ktay <kattie.tay@elasticpath.com>
2015-02-09 01:42:00 -0800

    Initial commit.

---
+++ src/com/ktay/ModernArtUI/MainActivity.java
@@ -0,0 +1,45 @@
+package com.ktay.ModernArtUI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+
+    private SeekBar seekBar;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initSeekBar();
+    }
+
+    /**
+     * Initialize the seekbar
+     */
+    private void initSeekBar() {
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
